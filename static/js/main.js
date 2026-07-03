5d878728b555597966d8a81bdf8518c40c650df3
RealRyanJames <realcoder2023@gmail.com>
2026-07-03 10:22:44 -0400

    Added Initial Button Code

--- static/js/main.js
+++ static/js/main.js
@@ -2,16 +2,12 @@
 function addedClickableButton() {
     const button = document.getElementById("button");
     const text = document.getElementById("text");
-    setButton(button, text);
-    // changeTextClickable();
+    button.addEventListener("click", () => {
+        text.textContent = `Coding For ${2026 - 2020}`;
+    });
 }
 // function changeTextClickable() {
 //   const text = document.getElementById("yearsCoding") as HTMLElement;
 //   text.textContent = `Coding For ${2026 - 2020}`;
 // }
-function setButton(btn, text) {
-    btn.addEventListener("click", () => {
-        text.textContent = `Coding For ${2026 - 2020}`;
-    });
-}
 addedClickableButton();
--- static/js/main.ts
+++ static/js/main.ts
@@ -1,19 +1,14 @@
 function addedClickableButton() {
   const button = document.getElementById("button") as HTMLButtonElement;
   const text = document.getElementById("text") as HTMLElement;
-  setButton(button, text);
-  // changeTextClickable();
+  button.addEventListener("click", () => {
+    text.textContent = `Coding For ${2026 - 2020}`;
+  });
 }
 
 // function changeTextClickable() {
 //   const text = document.getElementById("yearsCoding") as HTMLElement;
 //   text.textContent = `Coding For ${2026 - 2020}`;
 // }
-
-function setButton(btn: HTMLButtonElement, text: HTMLElement) {
-  btn.addEventListener("click", () => {
-    text.textContent = `Coding For ${2026 - 2020}`;
-  });
-}
 
 addedClickableButton();
