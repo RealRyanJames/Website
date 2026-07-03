5b1d4ba41831b7ea5f1d2fab64818419c7276d1b
RealRyanJames <realcoder2023@gmail.com>
2026-07-03 10:35:05 -0400

    input

--- main.sh
+++ main.sh
@@ -11,8 +11,11 @@ if $dev; then
         
         tsc ./static/js/main.ts
 
+        input="Development"
+        
+        read -p "Enter Command for Github" input
         git add .
-        git commit -m "Added Initial Button Code"
+        git commit -m input
         git push
         rm -rf node_modules
 
