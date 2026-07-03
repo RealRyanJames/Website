33069daf12530e76794c18d135e8e9e1a47463cc
RealRyanJames <realcoder2023@gmail.com>
2026-07-03 11:03:04 -0400

    input

--- main.sh
+++ main.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-isProd=1
-
-type="Development"
 dev=true
 
 if $dev; then 
-    
-        isProd = 0
-        
-        tsc ./static/js/main.ts
 
-        input="Development"
+    tsc ./static/js/main.ts
+    input="Development"
         
-        read -p "Enter Command for Github" input
-        git add .
-        git commit -m input
-        git push
-        rm -rf node_modules
+    read -p "Enter Command for Github" input
+    git add .
+    git commit -m input
+    git push
+    rm -rf node_modules
 
 fi
